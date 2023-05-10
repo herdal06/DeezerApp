07520b74a57a7c22f6632d5668b53700e5beff5e
herdal06 <h.erdal721@gmail.com>
2023-05-11 01:04:12 +0300

    Create use case for adding and removing tracks

---
+++ app/src/main/java/com/herdal/deezerapp/domain/usecase/AddOrRemoveTrackFromFavoriteUseCase.kt
@@ -0,0 +1,35 @@
+package com.herdal.deezerapp.domain.usecase
+
+import com.herdal.deezerapp.domain.repository.TrackRepository
+import com.herdal.deezerapp.domain.uimodel.Track
+import javax.inject.Inject
+
+class AddOrRemoveTrackFromFavoriteUseCase @Inject constructor(
+    private val trackRepository: TrackRepository,
+) {
+     /*suspend fun execute(track: Track) {
+        track.id?.let { id ->
+            if (trackRepository.isTrackFavorite(id)) {
+                trackRepository.deleteTrackFromFavorite(track.copy(isFavorite = false))
+            } else {
+                trackRepository.addTrackToFavorite(track.copy(isFavorite = true))
+            }
+        }
+    }
+
+      */
+
+
+   suspend fun execute(track: Track) {
+       track.id?.let { id ->
+           if (trackRepository.isTrackFavorite(id)) {
+               track.isFavorite = false
+               trackRepository.deleteTrackFromFavorite(track)
+           } else {
+               track.isFavorite = true
+               trackRepository.addTrackToFavorite(track)
+           }
+       }
+   }
+
+}
